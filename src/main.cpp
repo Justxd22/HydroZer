4e571a166eb3b5bafdf92e0c71a66e9d2fd61d22
justxd22 <xdjust18@gmail.com>
2025-12-16 20:53:30 +0200

    Fix mac address

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,42 @@
 #include <Arduino.h>
 #include <Ps3Controller.h>
 
-void setup() {
-    Serial.begin(9600);  
-    Ps3.begin("01:02:03:04:05:06"); 
-
-    Serial.println("PS3 Controller Initialized. Waiting for connection...");
+void setup()
+{
+  Serial.begin(115200);
+  Ps3.begin("9C:B6:D0:DD:9D:10");
+  Serial.println("PS3 Controller Initialized. Waiting for connection...");
 }
 
-void loop() {
-    if (Ps3.isConnected()) {
-        Serial.println("Controller Connected!");
+void loop()
+{
+  if (Ps3.isConnected())
+  {
+    Serial.println("Controller Connected!");
 
-        // Read Left Stick
-        int ly = Ps3.data.analog.stick.ly;
-        int lx = Ps3.data.analog.stick.lx;
-        
-        // Read Right Stick
-        int ry = Ps3.data.analog.stick.ry;
-        int rx = Ps3.data.analog.stick.rx;
+    // Read Left Stick
+    int ly = Ps3.data.analog.stick.ly;
+    int lx = Ps3.data.analog.stick.lx;
 
-        Serial.printf("LX: %4d, LY: %4d || RX: %4d, RY: %4d\n", lx, ly, rx, ry);
+    // Read Right Stick
+    int ry = Ps3.data.analog.stick.ry;
+    int rx = Ps3.data.analog.stick.rx;
 
-        if (Ps3.data.button.cross) Serial.println("Pressing Cross");
-        if (Ps3.data.button.square) Serial.println("Pressing Square");
-        if (Ps3.data.button.triangle) Serial.println("Pressing Triangle");
-        if (Ps3.data.button.circle) Serial.println("Pressing Circle");
+    Serial.printf("LX: %4d, LY: %4d || RX: %4d, RY: %4d\n", lx, ly, rx, ry);
 
-    } else {
-        Serial.println("Waiting for controller...");
-    }
-    
-    delay(100); // minimal delay to avoid flooding serial
+    if (Ps3.data.button.cross)
+      Serial.println("Pressing Cross");
+    if (Ps3.data.button.square)
+      Serial.println("Pressing Square");
+    if (Ps3.data.button.triangle)
+      Serial.println("Pressing Triangle");
+    if (Ps3.data.button.circle)
+      Serial.println("Pressing Circle");
+  }
+  else
+  {
+    Serial.println("Waiting for controller...");
+  }
+
+  delay(100);
 }
